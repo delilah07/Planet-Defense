74a14dc345177f3cbf34a44cb146bf621ac9a56a
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-07-21 19:25:33 +0200

    add debug and play rotation around Earth

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 class Planet {
   constructor(game) {
     this.game = game;
-    console.log(this.game);
     this.x = this.game.width / 2;
     this.y = this.game.height / 2;
     this.radius = 80;
@@ -13,9 +12,45 @@ class Planet {
       this.x - this.radius - 20,
       this.y - this.radius - 20
     );
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.stroke();
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      context.stroke();
+    }
+  }
+}
+
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width / 2;
+    this.y = this.game.height / 2;
+    this.radius = 40;
+    this.image = document.getElementById('player');
+    this.angle = 0;
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(this.image, -this.radius, -this.radius);
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(0, 0, this.radius, 0, Math.PI * 2);
+      context.stroke();
+    }
+
+    context.restore();
+  }
+  update() {
+    this.aim = this.game.calcAim(this.game.planet, this.game.mouse);
+    this.x =
+      this.game.planet.x +
+      (this.game.planet.radius + this.radius) * this.aim[0];
+    this.y =
+      this.game.planet.y +
+      (this.game.planet.radius + this.radius) * this.aim[1];
+    this.angle = Math.atan2(this.aim[3], this.aim[2]);
   }
 }
 
@@ -25,9 +60,40 @@ class Game {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.planet = new Planet(this);
+    this.player = new Player(this);
+    this.mouse = {
+      x: 0,
+      y: 0,
+    };
+    this.debug = true;
+
+    window.addEventListener('mousemove', (e) => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    });
+    window.addEventListener('keyup', (e) => {
+      if (e.key === 'd') this.debug = !this.debug;
+    });
   }
   render(context) {
     this.planet.draw(context);
+    this.player.draw(context);
+    this.player.update();
+
+    context.beginPath();
+    if (this.debug) {
+      context.moveTo(this.planet.x, this.planet.y);
+      context.lineTo(this.mouse.x, this.mouse.y);
+      context.stroke();
+    }
+  }
+  calcAim(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const aimX = (dx / distance) * -1;
+    const aimY = (dy / distance) * -1;
+    return [aimX, aimY, dx, dy];
   }
 }
 
@@ -40,5 +106,11 @@ window.addEventListener('load', () => {
   ctx.lineWidth = 2;
 
   const game = new Game(canvas);
-  game.render(ctx);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
 });
